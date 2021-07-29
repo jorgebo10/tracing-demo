46b1c54a61c2ee18a84895a403e338ab41ea8995
Jorge Bo <jorge.bo@naranjax.com>
2021-07-29 11:45:04 -0300

    Add tracing and jaeger config

--- src/main/java/com/example/tracingdemo/EShopController.java
+++ src/main/java/com/example/tracingdemo/EShopController.java
@@ -1,13 +1,28 @@
 package com.example.tracingdemo;
 
+import io.opentracing.Span;
+import io.opentracing.Tracer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EShopController {
+    private final Tracer tracer;
+
+    @Autowired
+    public EShopController(Tracer tracer) {
+        this.tracer = tracer;
+    }
 
     @GetMapping("/checkout")
     public String checkout() {
-        return "You have successfully checked out your shopping cart";
+        Span span = tracer.buildSpan("checkout").start();
+
+        String msg = "You have successfully checked out your shopping cart";
+
+        span.finish();
+
+        return msg;
     }
 }
