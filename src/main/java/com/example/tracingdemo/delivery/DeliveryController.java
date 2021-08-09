a59b09ca1ca9f60c21a5b01a1b1550ed70696e64
Jorge Bo <jorge.bo@naranjax.com>
2021-08-09 10:21:39 -0300

    propagate headers

--- src/main/java/com/example/tracingdemo/delivery/DeliveryController.java
+++ src/main/java/com/example/tracingdemo/delivery/DeliveryController.java
@@ -3,12 +3,12 @@ package com.example.tracingdemo.delivery;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Map;
 
 @RestController
 public class DeliveryController {
@@ -20,7 +20,8 @@ public class DeliveryController {
     }
 
     @RequestMapping("/arrangeDelivery")
-    public void arrangeDelivery(@RequestHeader Map<String, String> headers) {
-        restTemplate.postForLocation("http://logistics.tracing-demo:8080/transport", Void.class);
+    public void arrangeDelivery(@RequestHeader HttpHeaders httpHeaders) {
+        HttpEntity<String> entity = new HttpEntity<>("body", httpHeaders);
+        restTemplate.postForLocation("http://logistics.tracing-demo:8080/transport", entity, Void.class);
     }
 }
--- src/main/java/com/example/tracingdemo/shopping/EShopController.java
+++ src/main/java/com/example/tracingdemo/shopping/EShopController.java
@@ -2,7 +2,10 @@ package com.example.tracingdemo.shopping;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,10 +19,11 @@ public class EShopController {
     }
 
     @GetMapping("/checkout")
-    public String checkout() {
-        restTemplate.postForLocation("http://inventory.tracing-demo:8080/createOrder", Void.class);
-        restTemplate.postForLocation("http://billing.tracing-demo:8080/payment", Void.class);
-        restTemplate.postForLocation("http://delivery.tracing-demo:8080/arrangeDelivery", Void.class);
+    public String checkout(@RequestHeader HttpHeaders httpHeaders) {
+        HttpEntity<String> entity = new HttpEntity<>("body", httpHeaders);
+        restTemplate.postForLocation("http://inventory.tracing-demo:8080/createOrder", entity, Void.class);
+        restTemplate.postForLocation("http://billing.tracing-demo:8080/payment", entity, Void.class);
+        restTemplate.postForLocation("http://delivery.tracing-demo:8080/arrangeDelivery", entity, Void.class);
         return "You have successfully checked out your shopping cart";
     }
 }
